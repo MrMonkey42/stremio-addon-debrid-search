cb12226aca2d4aca3237cb71931452b740e367b6
MrMonkey42 <mrmonkey42@protonmail.com>
2023-10-27 21:32:22 +0530

    Add rate limiter on client IP

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
-import StremioAddonSdk from "stremio-addon-sdk"
 import express from 'express'
 import serverless from './serverless.js'
+import requestIp from 'request-ip'
+import rateLimit from 'express-rate-limit'
 
 const app = express()
+
+const rateLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hours
+    limit: 300, // Limit each IP to 300 requests per window
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    keyGenerator: (req) => requestIp.getClientIp(req)
+})
+app.use(rateLimiter)
 
 app.use((req, res, next) => serverless(req, res, next))
 app.listen(process.env.PORT || 55771, () => {
